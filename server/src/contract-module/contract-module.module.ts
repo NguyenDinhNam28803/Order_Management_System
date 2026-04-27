8d864f265d5e314cef955f2af7d7d52444506370
Nguyendinhnam28803 <nguyendinhnam241209@gmail.com>
2026-04-27 12:27:34 +0700

    Fix contracts email to supplier part 2

--- server/src/contract-module/contract-module.module.ts
+++ server/src/contract-module/contract-module.module.ts
@@ -4,9 +4,10 @@ import { ContractModuleController } from './contract-module.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { NotificationModuleModule } from '../notification-module/notification-module.module';
 import { ApprovalModuleModule } from '../approval-module/approval-module.module';
+import { ExternalTokenModule } from '../external-token-module/external-token.module';
 
 @Module({
-  imports: [PrismaModule, NotificationModuleModule, ApprovalModuleModule],
+  imports: [PrismaModule, NotificationModuleModule, ApprovalModuleModule, ExternalTokenModule],
   controllers: [ContractModuleController],
   providers: [ContractModuleService],
   exports: [ContractModuleService],
